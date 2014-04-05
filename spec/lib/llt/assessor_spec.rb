8dd4413432fd95c577ae15579ef84a3feb15f53d
LFDM <1986gh@gmail.com>
2014-04-05 15:48:06 +0200

    Move and fix default spec

--- spec/lib/llt/assessor_spec.rb
+++ spec/lib/llt/assessor_spec.rb
@@ -4,8 +4,4 @@ describe LLT::Assessor do
   it 'should have a version number' do
     LLT::Assessor::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should eq(true)
-  end
 end
